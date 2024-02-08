a2ffc56f121cd83d368ab846c126de4215173121
VarunChowdary0 <saivarunchowdarypoludasu4248@gmail.com>
2024-02-08 20:30:14 +0530

    km

--- client/src/Components/Regiter.tsx
+++ client/src/Components/Regiter.tsx
@@ -110,7 +110,7 @@ const Regiter:React.FC = () => {
                     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
                     <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                         <h2 className="mt-10 text-center text-2xl font-bold 
-                        leading-9 tracking-tight text-gray-900">Welcome to ***** </h2>
+                        leading-9 tracking-tight text-gray-900">Welcome to zen browse </h2>
                     </div>
 
                     <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
